602e442d14697a6fabcef3bd254e179ea101e7c6
Harish <harishgan@gmail.com>
2017-08-08 23:11:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,5 +13,65 @@ button.onclick = function () {
     if (request.readystate === XMLHttp.Done) {
         
     }
-}
-console.log('Loaded!');
+
+        
+        
+        
+        
+        
+     // Make the request
+     request.open('GET', 'http://harishgan.imad.hasura-app.io/counter', true);
+     request.send(null);   
+    };
+    
+    // Submit Name
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function ();
+    
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store in a variable
+    request.onreadystatechange = function() {
+    if (request.readystate === XMLHttp.Done) {
+       //Take some action
+       if(request.status === 200);
+            // Capture a list of names and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for var i = 0; i<name.length; i++;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+   
+   
+    };
