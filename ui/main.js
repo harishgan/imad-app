7380fc587938a615d72ba73c827d4eabff7bbc08
Harish <harishgan@gmail.com>
2017-08-08 22:48:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
+// Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+    
+    //Make a request to the counter end point
+    var request = new XMLHttpRequest();
+    
+    
+    //Capture the response and store in a variable
+    request.onreadystatechange = function() {
+    if (request.readystate === XMLHttp.Done) {
+        
+    }
+}
 console.log('Loaded!');
