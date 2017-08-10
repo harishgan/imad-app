fc850e8cdacbd91d3614e047f76e3c2942f4aebd
Harish <harishgan@gmail.com>
2017-08-10 21:33:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,77 +1,12 @@
-// Counter Code
-var button = document.getElementById('counter');
-var counter = 0;
+console.log('Loaded!'); 
 
-button.onclick = function () {
-    
-    //Make a request to the counter end point
-    var request = new XMLHttpRequest();
-    
-    
-    //Capture the response and store in a variable
-    request.onreadystatechange = function() {
-    if (request.readystate === XMLHttp.Done) {
-        
-    }
+//Change the text of the main-text div
+var element = document.getElementById('main-text');
 
-        
-        
-        
-        
-        
-     // Make the request
-     request.open('GET', 'http://harishgan.imad.hasura-app.io/counter', true);
-     request.send(null);   
-    };
-    
-    // Submit Name
-    var submit = document.getElementById('submit_btn');
-    submit.onclick = function ();
-    
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store in a variable
-    request.onreadystatechange = function() {
-    if (request.readystate === XMLHttp.Done) {
-       //Take some action
-       if(request.status === 200);
-            // Capture a list of names and render it as a list
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            for var i = 0; i<name.length; i++;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-   
-   
-    };
+element.innerHTML = 'New Value';
+
+//Move the image
+var img = document. getElementById('img');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
